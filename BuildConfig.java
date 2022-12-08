a6f5c49e6367f83fed28535ed36003187b780293
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-12-08 09:33:35 +0700

    Update BuildConfig.java

--- BuildConfig.java
+++ BuildConfig.java
@@ -2,7 +2,7 @@
  * Automatically generated file. DO NOT MODIFY
  */
 package com.example.android.devbyteviewer;
-
+//Buil config ini berfungsi intuk mengkonfigurasi dan melakukan proses pada aplikasi agar dapat berjalan
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.example.android.devbyteviewer";
